70ec1579775962b712b8fb92fadb534e34357496
khanindradeka1993 <118038308+khanindradeka1993@users.noreply.github.com>
2026-05-20 10:18:58 +0530

    Refactor connection logic and remove unused code

--- app.js
+++ app.js
@@ -1,92 +1,36 @@
-const connectBtn = document.getElementById("connectBtn");
-const sendBtn = document.getElementById("sendBtn");
-const statusText = document.getElementById("status");
+const connectBtn =
+document.getElementById("connectBtn");
 
-let signer;
-
-const USDC_ADDRESS =
-"0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";
-
-const ABI = [
-  "function transfer(address to, uint amount) returns (bool)"
-];
+const statusText =
+document.getElementById("status");
 
 connectBtn.onclick = async () => {
 
   try {
 
-    if (typeof window.ethereum !== "undefined") {
-
-      await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      const provider =
-        new ethers.providers.Web3Provider(window.ethereum);
-
-      signer = provider.getSigner();
-
-      const address =
-        await signer.getAddress();
+    if (!window.ethereum) {
 
       statusText.innerText =
-        "Connected: " +
-        address.slice(0,6) +
-        "..." +
-        address.slice(-4);
+      "MetaMask not found";
 
-    } else {
-
-      alert("MetaMask not detected");
-
+      return;
     }
+
+    const accounts =
+    await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    statusText.innerText =
+    "Connected: " +
+    accounts[0];
 
   } catch (err) {
 
     console.log(err);
 
     statusText.innerText =
-      "Wallet connection failed";
+    "Wallet connection failed";
 
-  }
-};
-
-sendBtn.onclick = async () => {
-
-  try {
-
-    const receiver =
-      document.getElementById("receiver").value;
-
-    const amount =
-      document.getElementById("amount").value;
-
-    const contract =
-      new ethers.Contract(
-        USDC_ADDRESS,
-        ABI,
-        signer
-      );
-
-    const tx =
-      await contract.transfer(
-        receiver,
-        ethers.utils.parseUnits(amount,6)
-      );
-
-    statusText.innerText =
-      "Transaction Sent 🚀";
-
-    await tx.wait();
-
-    statusText.innerText =
-      "Payment Successful ✅";
-
-  } catch(err){
-
-    console.log(err);
-
-    statusText.innerText =
-      "Transaction Failed ❌";
   }
 };
