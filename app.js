847505fa487f5844f37998d2d00cbbda293e92c7
khanindradeka1993 <118038308+khanindradeka1993@users.noreply.github.com>
2026-05-20 13:48:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+const USDC =
+"0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";
+
+const ABI = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "type": "function"
+  }
+];
+
 document
 .getElementById("connectBtn")
 .onclick = async () => {
@@ -7,46 +30,54 @@ document
 
   try {
 
-    if (window.ethereum) {
-
-      const accounts =
-      await window.ethereum.request({
-        method: "eth_requestAccounts"
-      });
-
-      const address = accounts[0];
-
-      const balanceHex =
-      await window.ethereum.request({
-        method: "eth_getBalance",
-        params: [address, "latest"]
-      });
-
-      const balance =
-      parseInt(balanceHex,16) / 1e18;
-
-      status.innerText =
-      "Connected: " +
-      address.slice(0,6) +
-      "..." +
-      address.slice(-4) +
-      "\nBalance: " +
-      balance.toFixed(4) +
-      " ETH";
-
-    } else {
+    if (!window.ethereum) {
 
       status.innerText =
       "MetaMask not detected";
 
+      return;
     }
 
-  } catch (e) {
+    const accounts =
+    await window.ethereum.request({
+      method: "eth_requestAccounts"
+    });
 
-    console.log(e);
+    const address =
+    accounts[0];
+
+    const provider =
+    new ethers.providers.Web3Provider(
+      window.ethereum
+    );
+
+    const contract =
+    new ethers.Contract(
+      USDC,
+      ABI,
+      provider
+    );
+
+    const balance =
+    await contract.balanceOf(address);
+
+    const formatted =
+    Number(balance) / 1000000;
 
     status.innerText =
-    "Connection failed";
+    "Connected: " +
+    address.slice(0,6) +
+    "..." +
+    address.slice(-4) +
+    "\nUSDC Balance: " +
+    formatted;
+
+  } catch(err) {
+
+    console.log(err);
+
+    status.innerText =
+    "Connection Failed";
 
   }
 
