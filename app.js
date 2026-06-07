756b55754c6609a7405608737ed97ea1310ebe5c
khanindradeka1993 <118038308+khanindradeka1993@users.noreply.github.com>
2026-06-07 10:08:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,35 +73,3 @@ if (faucetBtn) {
     window.location.href = "https://faucet.circle.com";
   });
 }
-const refreshBtn = document.getElementById("refreshBtn");
-const balanceEl = document.getElementById("balance");
-
-refreshBtn.addEventListener("click", async () => {
-  try {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts"
-    });
-
-    const address = accounts[0];
-
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    const usdcContract = new ethers.Contract(
-      "0x3600000000000000000000000000000000000000",
-      [
-        "function balanceOf(address owner) view returns (uint256)"
-      ],
-      provider
-    );
-
-    const balance = await usdcContract.balanceOf(address);
-
-    balanceEl.innerText =
-      (Number(balance) / 1000000).toFixed(2) + " USDC";
-
-catch (err) {
-  console.error(err);
-  alert(JSON.stringify(err));
-}
-});
-  
